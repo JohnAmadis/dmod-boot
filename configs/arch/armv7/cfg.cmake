e3e8e136ca8bc1f3327ffca768fcafea374f1b81
Patryk Kubiak <patryk.kubiak90@gmail.com>
2025-11-01 18:49:44 +0100

    added support for few more architectures

--- configs/arch/armv7/cfg.cmake
+++
@@ -1,7 +0,0 @@
-
-# ======================================================================
-#               DMOD Configuration
-# ======================================================================
-
-# Name of the tools configuration
-set(DMOD_TOOLS_NAME	"arch/armv7" CACHE STRING "Name of the tools configuration")
