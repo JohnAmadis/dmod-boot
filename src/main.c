aad3869aadf11982fe7f6a2faf2e8793f1c5a991
Patryk Kubiak <patryk.kubiak90@gmail.com>
2025-11-01 19:33:14 +0100

    Add DMLoG logging support and update project configuration
    - Introduced DMLoG submodule and updated CMakeLists to include it.
    - Modified .gitmodules to reference DMLoG repository.
    - Updated VS Code configuration for DMLoG include paths.
    - Enhanced linker script to allocate space for logs.
    - Implemented logging functionality in main.c using DMLoG.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
 #include "dmod.h"
+#include "dmlog.h"
 
-int main(int argc, char** argv) {
-    while(1);
+extern void* __logs_start__;
+extern void* __logs_end__;
+
+void delay(int cycles)
+{
+    for(volatile int i = 0; i < cycles; i++);
+}
+
+int main(int argc, char** argv) 
+{
+    void* logs_start = &__logs_start__;
+    void* logs_end = &__logs_end__;
+    dmlog_index_t  logs_size = (dmlog_index_t)((uintptr_t)logs_end - (uintptr_t)logs_start);
+    
+    dmlog_ctx_t ctx = dmlog_create(logs_start, logs_size);
+
+    while(1)
+    {
+        dmlog_puts(ctx, "Hello, DMLoG!");
+        delay(1000000);
+    }
     return 0;
 }
