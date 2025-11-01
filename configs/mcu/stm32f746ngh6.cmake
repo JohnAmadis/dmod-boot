07c840eb65ff10dce3466f1f4766a33620dca971
Patryk Kubiak <patryk.kubiak90@gmail.com>
2025-11-01 14:18:55 +0100

    Update CMake configuration for STM32F746 target and add architecture support

--- configs/mcu/stm32f746ngh6.cmake
+++ configs/mcu/stm32f746ngh6.cmake
@@ -1,9 +1,10 @@
 # ======================================================================
 #               Configuration
 # ======================================================================
-set(DMBOOT_MCU_NAME "stm32f746" CACHE STRING "Name of the target microcontroller")
+set(DMBOOT_MCU_NAME "stm32f746ngh6" CACHE STRING "Name of the target microcontroller")
+set(DMBOOT_MCU_SERIES "stm32f7" CACHE STRING "Series of the target microcontroller")
 set(DMBOOT_ARCH "armv7" CACHE STRING "Architecture of the target microcontroller")
-set(DMBOOT_MCU_FAMILY "cortex-m7" CACHE STRING "Microcontroller family")
+set(DMBOOT_ARCH_FAMILY "cortex-m7" CACHE STRING "Microcontroller family")
 
 # ======================================================================
 #               DMOD Configuration
@@ -11,5 +12,10 @@ set(DMBOOT_MCU_FAMILY "cortex-m7" CACHE STRING "Microcontroller family")
 # Name of the target cpu (if empty, the target is generic)
 set(DMOD_CPU_NAME   ${DMBOOT_MCU_NAME} CACHE STRING "Name of the target cpu, if empty, the target is generic")
 
+# ======================================================================
+#               Include architecture configuration
+# ======================================================================
+include(configs/arch/${DMBOOT_ARCH}/cfg.cmake)
+include(configs/arch/${DMBOOT_ARCH}/${DMBOOT_ARCH_FAMILY}/cfg.cmake)
 
 
