59fd957be8af37e0099328d5a5dd19ab4b4c1d02
Patryk Kubiak <patryk.kubiak90@gmail.com>
2025-11-01 17:25:58 +0100

    Add OpenOCD integration for firmware installation and connection

--- configs/arch/armv7/cfg.cmake
+++ configs/arch/armv7/cfg.cmake
@@ -5,4 +5,3 @@
 
 # Name of the tools configuration
 set(DMOD_TOOLS_NAME	"arch/armv7" CACHE STRING "Name of the tools configuration")
-
--- configs/mcu/STM32F746xG.cmake
+++ configs/mcu/STM32F746xG.cmake
@@ -7,6 +7,13 @@ set(DMBOOT_ARCH "armv7" CACHE STRING "Architecture of the target microcontroller
 set(DMBOOT_ARCH_FAMILY "cortex-m7" CACHE STRING "Microcontroller family")
 
 # ======================================================================
+#               OpenOCD Configuration
+# ======================================================================
+set(OPENOCD_INTERFACE "interface/stlink.cfg")
+find_program(OPENOCD openocd REQUIRED)
+set(OPENOCD_TARGET "target/stm32f7x.cfg")
+
+# ======================================================================
 #               DMOD Configuration
 # ======================================================================
 # Name of the target cpu (if empty, the target is generic)
