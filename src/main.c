349b277f5403c6a95b1f80a0b33bd059116a61c8
Patryk Kubiak <patryk.kubiak90@gmail.com>
2025-11-05 21:59:35 +0100

    Enhance logging and error handling in main loop; update linker script and subproject commits

--- src/main.c
+++ src/main.c
@@ -4,10 +4,19 @@
 
 extern void* __logs_start__;
 extern void* __logs_end__;
+extern void* __dmod_inputs_start;
+extern void* __dmod_inputs_end;
 
 void delay(int cycles)
 {
     for(volatile int i = 0; i < cycles; i++);
+}
+
+void HardFault_Handler(void)
+{
+    dmlog_ctx_t ctx = dmlog_get_default();
+    dmlog_puts(ctx, "HardFault detected!\n");
+    while(1);
 }
 
 int main(int argc, char** argv) 
@@ -21,13 +30,22 @@ int main(int argc, char** argv)
     dmlog_ctx_t ctx = dmlog_create(logs_start, logs_size);
     dmlog_set_as_default(ctx);
 
-    dmlog_puts(ctx, "Hello dmod-boot!\n");
+    dmlog_puts(ctx, "DMOD-Boot started\n");
+
+    void* inputs_start = &__dmod_inputs_start;
+    void* inputs_end = &__dmod_inputs_end;
+    size_t inputs_size = (size_t)((uintptr_t)inputs_end - (uintptr_t)inputs_start);
+
+    DMOD_LOG_INFO("Inputs start address: %u\n", (uintptr_t)inputs_start);
+    DMOD_LOG_INFO("Inputs size: %u bytes\n", (unsigned int)inputs_size);
+
+    Dmod_Initialize();
 
     int i = 0;
     while(1)
     {
-        Dmod_Printf("Welcome for %d times\n", i++);
-        //delay(10000);
+        DMOD_LOG_INFO("Waiting for better times... ID: %d\n", i++);
+        delay(1000000);
     }
     return 0;
 }
